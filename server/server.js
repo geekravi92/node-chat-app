4de16d724648411dc63a44db6c2f14dd509956f3
Arvind Jaiswal <ravisonic100@gmail.com>
2018-02-06 05:31:13 -0500

    Greetings of new users and alert to the others

--- server/server.js
+++ server/server.js
@@ -17,15 +17,50 @@ io.on('connection', socket => {
     socket.on("createMessage", (msgData) => {
         console.log(msgData);
 
-        // socket.emit emits to the single connection
+        /**
+         * socket.emit emits to the single connection
+         */
         // socket.emit("newMessage", msgData);
 
-        // io.emit emits to every single connection
-        io.emit("newMessage", {
-            from: msgData.from,
-            text: msgData.text,
+
+        /**
+         * io.emit emits to every single connection
+         */
+        // io.emit("newMessage", {
+        //     from: msgData.from,
+        //     text: msgData.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+        /**
+         * socket.broadcast emits to everyone but originator
+         */
+        // socket.broadcast.emit("newMessage", {
+        //     from: msgData.from,
+        //     text: msgData.text,
+        //     createdAt: new Date().getTime()
+        // });
+
+
+
+
+        /**
+         * Chat App Functionalities
+         */
+
+        // socket.emit for Welcome Message to the Joinee
+        socket.emit("newMessage", {
+            from: "Admin",
+            text: `Welcome to the Node Chat App, ${msgData.from}`
+        });
+
+        // socket.broadcast to intimate everyone else that a new user has joined
+        socket.broadcast.emit("newMessage", {
+            from: "Admin",
+            text: `${msgData.from} has joined the group`,
             createdAt: new Date().getTime()
         });
+        //      
     });
 
 
